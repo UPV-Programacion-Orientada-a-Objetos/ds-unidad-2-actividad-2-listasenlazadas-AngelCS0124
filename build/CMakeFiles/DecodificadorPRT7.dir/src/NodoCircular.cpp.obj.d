b179a1015b4049951546f18974ac3ce8aa5823ab
Angel Gabriel Coronado Sánchez <2430124@upv.edu.mx>
2025-11-06 17:44:09 -0600

    Corrccion de gitignore 2

--- build/CMakeFiles/DecodificadorPRT7.dir/src/NodoCircular.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/DecodificadorPRT7.dir/src/NodoCircular.cpp.obj: \
- C:\Users\as_co\C\ds-unidad-2-actividad-2-listasenlazadas-AngelCS0124\src\NodoCircular.cpp \
- C:/Users/as_co/C/ds-unidad-2-actividad-2-listasenlazadas-AngelCS0124/include/NodoCircular.h
